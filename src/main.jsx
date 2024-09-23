df230e4f5d703cb7b2b6cd0af91194e05a30736c
KezroXV <kezro10@gmail.com>
2024-09-24 00:55:52 +0100

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,45 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import AboutPage from './Pages/AboutPage.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ContactPage from './Pages/ContactPage.jsx'
+import BrandsPage from './Pages/BrandsPage.jsx'
+import BlogPage from './Pages/BlogPage.jsx'
+import CareersPage from './Pages/CareersPage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "Brands",
+    element: <BrandsPage/>,
+  },
+  {
+    path: "About",
+    element: <AboutPage/>,
+  },
+  {
+    path: "Blog",
+    element: <BlogPage></BlogPage>,
+  },
+  {
+    path: "Contact",
+    element: <ContactPage/>,
+  },
+  {
+    path: "Careers",
+    element: <CareersPage/>,
+  },
+]);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+   <RouterProvider router={router}/>
+  </StrictMode>,
+)
